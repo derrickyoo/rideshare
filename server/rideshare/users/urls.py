61dd1bd2accf1f6f31e9be7456575ab10af559b4
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-09-11 14:14:14 -0700

    Add log in api with rest framework simplejwt

--- server/rideshare/users/urls.py
+++ server/rideshare/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import SignUpView
+from .views import SignUpView, LogInView
 
 
 app_name = "users"
 urlpatterns = [
     path("sign_up/", SignUpView.as_view(), name="sign_up"),
+    path("log_in/", LogInView.as_view(), name="log_in"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
