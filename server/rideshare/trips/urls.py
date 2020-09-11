506e10aee78d8414c6f743e4a44f91a27d599b23
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-09-11 15:24:39 -0700

    Retrieve details of single trip

--- server/rideshare/trips/urls.py
+++ server/rideshare/trips/urls.py
@@ -5,4 +5,9 @@ from .views import TripView
 app_name = "trips"
 urlpatterns = [
     path("", TripView.as_view({"get": "list"}), name="trip_list"),
+    path(
+        "<uuid:trip_id>/",
+        TripView.as_view({"get": "retrieve"}),
+        name="trip_detail",
+    ),
 ]
--- server/rideshare/trips/views.py
+++ server/rideshare/trips/views.py
@@ -5,6 +5,8 @@ from .serializers import TripSerializer
 
 
 class TripView(viewsets.ReadOnlyModelViewSet):
+    lookup_field = "id"
+    look_up_rule_kwargs = "trip_id"
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
