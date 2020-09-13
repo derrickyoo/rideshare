0eac353236a297eaab3550c878d0bcfe223d3e3b
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-09-13 15:03:02 -0700

    Add client landing page

--- client/src/app/components/landing/landing.component.ts
+++ client/src/app/components/landing/landing.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService, IUser } from '../../services/auth.service';
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
+
+  getUser(): IUser {
+    return AuthService.getUser();
+  }
+
+  logOut(): void {
+    this.authService.logOut();
+  }
 }
