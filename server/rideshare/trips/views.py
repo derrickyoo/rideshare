9414a9581fac16bc1c165ba898ad0760eb369f35
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-09-15 14:03:14 -0700

    Add queryset to trips view

--- server/rideshare/trips/views.py
+++ server/rideshare/trips/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import permissions, viewsets
 
 from .models import Trip
@@ -10,3 +11,13 @@ class TripView(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
+
+    def get_queryset(self):  # new
+        user = self.request.user
+        if user.group == "driver":
+            return Trip.objects.filter(
+                Q(status=Trip.REQUESTED) | Q(driver=user)
+            )
+        if user.group == "rider":
+            return Trip.objects.filter(rider=user)
+        return Trip.objects.none()
