780943fe703d6ccbf779a05825ffc24f0c0a1922
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-09-15 13:56:48 -0700

    Update custom user to inherit time stamped model

--- server/rideshare/users/models.py
+++ server/rideshare/users/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from model_utils.models import TimeStampedModel
 
 
-class CustomUser(AbstractUser):
-    pass
+class CustomUser(AbstractUser, TimeStampedModel):
+    @property
+    def group(self):
+        groups = self.groups.all()
+        return groups[0].name if groups else None
